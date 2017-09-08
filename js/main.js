fe875978ee2f3a37f33baa62733536e217797776
Rupesh Bhatti <rupesh.bhatti@hotmail.com>
2017-09-08 15:12:02 +0100

    Created game logic

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Output a message to the console so we know that index.html is correctly calling main.js
+
+console.log("Up and Running!");
+
+// Initialise variables: two king cards and two queen cards
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+// Display two cards the user has flipped in the console
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
