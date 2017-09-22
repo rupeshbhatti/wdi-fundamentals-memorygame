9d6994c8acc00797033370a58139ad88008f1cb5
Rupesh Bhatti <rupesh.bhatti@hotmail.com>
2017-09-22 13:41:00 +0100

    Code refactoring

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-/* Output a message to the console so we know that index.html is correctly calling main.js
-console.log("Up and Running!");*/
-
 // Inialise a new array of card objects and store two queen cards and two king cards
 var cards = [
   {
@@ -35,111 +32,120 @@ var cards = [
 // Create a new empty array to hold the cards in play
 var cardsInPlay = [];
 
-// function for updating the scoreboard
-var updateScore = function() {
-    // Get the current score
+//-------------------------------------------------------------------------------------------------------------------------------
+/* A Function for updating the scoreboard:
+    - increment the current score and write it back to the score element
+*/
+function updateScore() {
     var currentScore = document.getElementById('score').innerHTML;
 
-    // Increment the current score
     currentScore ++;
-
-    //Write the new score back to the DOM
     document.getElementById('score').innerHTML = currentScore;
 }
-
-// Function that checks both card objects in the cardsInPlay array for a match
-var checkForMatch = function () {
-
-    // check for a match using the rank property of each card object in the cardsInPlay array
+//-------------------------------------------------------------------------------------------------------------------------------
+// A Function that updates the game-result
+function updateGameResult(result, color){
+  document.getElementById('game-result').innerHTML = result;
+  document.getElementById('game-result').style.color = color;
+}
+//-------------------------------------------------------------------------------------------------------------------------------
+// A Function that sets the class of the flipped cards so that the border-color can be updated to red (cardNoMatch) / green (cardWithMatch)
+function updateBorderColor(clsName,index){
+  document.getElementsByTagName('img')[cardsInPlay[index].position].className = clsName;
+}
+//-------------------------------------------------------------------------------------------------------------------------------
+// A Function that unhides the play again button
+function unhidePlayAgainButton(){
+  document.querySelector('.hidden').removeAttribute('class');
+}
+//-------------------------------------------------------------------------------------------------------------------------------
+// A Function that re-hides the play again button
+function hidePlayAgainButton(){
+  document.getElementById('play-again').className = 'hidden';
+}
+//-------------------------------------------------------------------------------------------------------------------------------
+/* A Function that checks both card objects in cardsInPlay[] for a match:
+    - check for a match using the rank property of each card object in cardsInPlay[]
+    - if a match is found: set the class for the flipped cards to cardWithMatch so that the card border color is updated to green
+    - if a match isn't found: set the class for the flipped cards to cardNoMatch so that the card border color is updated to red
+    - update the game-result with either 'You found a match!' or 'Better luck next time'
+*/
+function checkForMatch() {
     if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
-
-      //select the flipped card objects and set class to cardWithMatch so that the card border color is updated to green
       for (var i = 0; i < cardsInPlay.length; i++) {
-        document.getElementsByTagName('img')[cardsInPlay[i].position].className = 'cardWithMatch';
+        updateBorderColor('cardWithMatch',i);
       }
 
-      //set p value for game-result to contain result
-      document.getElementById('game-result').innerHTML = 'You found a match!';
-      document.getElementById('game-result').style.color = "green";
-
+      updateGameResult('You found a match!','green');
       updateScore();
 
     } else {
 
-      //select the flipped card objects and set class to cardNoMatch so that the card border color is updated to red
       for (var i = 0; i < cardsInPlay.length; i++) {
-        document.getElementsByTagName('img')[cardsInPlay[i].position].className = 'cardNoMatch';
+        updateBorderColor('cardNoMatch',i);
       }
 
-      //set p value for game-result to contain result
-      document.getElementById('game-result').innerHTML = 'Better luck next time';
-      document.getElementById('game-result').style.color = "red";
+      updateGameResult('Better luck next time','red');
     }
 }
-
-// A function to flip the card that has been clicked
-var flipCard = function () {
-  //only flip a card if two cards haven't already been flipped
+//-------------------------------------------------------------------------------------------------------------------------------
+/* A function to flip the card that has been clicked:
+    - clicked card is only flipped if two cards haven't already been flipped
+    - flip the card that has been clicked by setting the src attribute to the card image
+    - push the flipped card to cardsInPlay[] if it hasn't already been flipped i.e. doesn't already exist in cardsInPlay[]
+    - if two cards have been flipped, call checkForMatch() and unhide the play again button
+*/
+function flipCard() {
   if (cardsInPlay.length !== 2) {
-      //discover which card was flipped by User
       var cardId = this.getAttribute('data-id');
 
-      // display the front of the card that has been flipped
       this.setAttribute('src', cards[cardId].cardImage);
 
-      // push the flipped card to the cardsInPlay array if it hasn't already been flipped over
       if (cardsInPlay[0] !== cards[cardId] ) {
         cardsInPlay.push(cards[cardId]);
       }
 
-      // if two cards have been flipped, call checkForMatch()
       if (cardsInPlay.length === 2) {
         checkForMatch();
-        // show the hidden 'Play Again button'
-        document.querySelector('.hidden').removeAttribute('class');
+        unhidePlayAgainButton();
       }
   }
-
 }
-
-// A function to create the board
-var createBoard = function () {
-
-    //loop through the cards array
+//-------------------------------------------------------------------------------------------------------------------------------
+/* A function to create the board:
+      - loop through the cards array
+      - create an img element and set attribute src to the image of the back of the card
+      - the data-id to the index i
+      - set event listener which will call flipCard when a card is clicked
+*/
+function createBoard() {
     for (var i = 0 ; i < cards.length ; i++) {
-      //create an img element and store in a new variable
       var cardElement = document.createElement('img');
-      //set src attribute for the new cardElement
+
       cardElement.setAttribute('src', 'images/back.png');
-      //set data-id attribute for the new cardElement
       cardElement.setAttribute('data-id',i);
-      //add a click event listener to the cardElement - when clicked, call flipCard function
       cardElement.addEventListener('click', flipCard);
-      //append new cardElement i.e. img with src / data-id attributes to the game-board DOM node
       document.getElementById('game-board').appendChild(cardElement);
     }
 }
-
-var playAgain = function () {
-  //empty the game-result message
-  document.getElementById('game-result').innerHTML = '';
-
+//-------------------------------------------------------------------------------------------------------------------------------
+/* A function that allows the user to play again:
+    - empty the game-result message
+    - remove border shading of cards in play
+    - unflip cards in play
+    - empty the cardsInPlay array
+    - Hide the replay button
+*/
+function playAgain() {
+  updateGameResult('','');
   for (var i = 0; i < cardsInPlay.length; i++) {
-    //remove border shading of cards in play
     document.getElementsByTagName('img')[cardsInPlay[i].position].className = '';
-
-    //unflip cards in play
     document.getElementsByTagName('img')[cardsInPlay[i].position].setAttribute('src', 'images/back.png');
   }
-
-	//empty the cardsInPlay array
   cardsInPlay = [];
-
-	//hide the replay button
-  document.getElementById('play-again').className = 'hidden';
+  hidePlayAgainButton();
 }
-
-//-----------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------
 
 //add a click event listener to the play again button - when clicked, call createBoard function
 document.getElementById('play-again').addEventListener('click', playAgain);
